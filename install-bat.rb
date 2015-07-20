6421880eeb5e602b94dd63f068ca368e20305589
Ben_Moss <bmoss@pivotal.io>
2015-07-20 14:08:41 -0400

    Add an integration test for the batch script generator
    [#99416710]
    
    Signed-off-by: John_Shahid <jshahid@pivotal.io>

--- install-bat.rb
+++ install-bat.rb
@@ -5,8 +5,11 @@ require 'json'
 require 'yaml'
 require 'fileutils'
 
+BOSH_URL = ARGV[0]
+OUTPUT_DIR = ARGV[1]
+
 def getJSON(path)
-  url = "https://192.168.50.4:25555#{path}"
+  url = BOSH_URL + path
   JSON.parse(open(url, :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE, :http_basic_authentication=>["admin", "admin"]).read)
 end
 
@@ -15,7 +18,8 @@ deployment = deployments.detect do |d|
   (%w(cf diego) - d["releases"].map{|r|r["name"]}).empty?
 end["name"]
 
-manifest = YAML.load(getJSON("/deployments/#{deployment}")["manifest"]).to_hash
+manifest = getJSON("/deployments/#{deployment}")["manifest"]
+manifest = YAML.load(manifest).to_hash
 consul_ips = manifest["properties"]["consul"]["agent"]["servers"]["lan"].join(',')
 # etcd_cluster="https://etcd.service.consul:4001"
 cf_etcd_cluster = manifest["properties"]["etcd"]["machines"].map{|ip|"http://#{ip}:4001"}.first
@@ -26,39 +30,37 @@ end.select{|a|a}.uniq
 loggregator_shared_secret=manifest["properties"]["loggregator_endpoint"]["shared_secret"]
 
 puts "Generate files in output directory"
-FileUtils.mkdir_p "output"
+FileUtils.mkdir_p OUTPUT_DIR
 
 certs = manifest["properties"]["diego"]["etcd"]
-puts "  output/ca.crt"
-open("output/ca.crt", 'w') do |f|
+puts "  #{OUTPUT_DIR}/ca.crt"
+open("#{OUTPUT_DIR}/ca.crt", 'w') do |f|
   f.write certs["ca_cert"]
 end
-puts "  output/client.crt"
-open("output/client.crt", 'w') do |f|
+puts "  #{OUTPUT_DIR}/client.crt"
+open("#{OUTPUT_DIR}/client.crt", 'w') do |f|
   f.write certs["client_cert"]
 end
-puts "  output/client.key"
-open("output/client.key", 'w') do |f|
+puts "  #{OUTPUT_DIR}/client.key"
+open("#{OUTPUT_DIR}/client.key", 'w') do |f|
   f.write certs["client_key"]
 end
 
-etcd_ca_file='%cd%\ca.crt'
-
 zones.each do |zone|
-  puts "  output/install_#{zone}.bat"
-  open("output/install_#{zone}.bat", 'w') do |f|
+  puts "  #{OUTPUT_DIR}/install_#{zone}.bat"
+  open("#{OUTPUT_DIR}/install_#{zone}.bat", 'w') do |f|
     f.write <<-BAT
-msiexec /norestart /i diego.msi
-  ADMIN_USERNAME=[USERNAME]
-  ADMIN_PASSWORD=[PASSWORD]
-  CONSUL_IPS=#{consul_ips}
-  CF_ETCD_CLUSTER=#{cf_etcd_cluster}
-  STACK=windows2012R2
-  REDUNDANCY_ZONE=#{zone}
-  LOGGREGATOR_SHARED_SECRET=#{loggregator_shared_secret}
-  ETCD_CA_FILE=%cd%\\ca.crt
-  ETCD_CERT_FILE=%cd%\\client.crt
-  ETCD_KEY_FILE=%cd%\\client.key
+msiexec /norestart /i diego.msi ^\r
+  ADMIN_USERNAME=[USERNAME] ^\r
+  ADMIN_PASSWORD=[PASSWORD] ^\r
+  CONSUL_IPS=#{consul_ips} ^\r
+  CF_ETCD_CLUSTER=#{cf_etcd_cluster} ^\r
+  STACK=windows2012R2 ^\r
+  REDUNDANCY_ZONE=#{zone} ^\r
+  LOGGREGATOR_SHARED_SECRET=#{loggregator_shared_secret} ^\r
+  ETCD_CA_FILE=%cd%\\ca.crt ^\r
+  ETCD_CERT_FILE=%cd%\\client.crt ^\r
+  ETCD_KEY_FILE=%cd%\\client.key \r
 BAT
   end
 end
---
+++ integration/generate_test.go
@@ -0,0 +1,183 @@
+package integration_test
+
+import (
+	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"path/filepath"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
+	"github.com/onsi/gomega/ghttp"
+)
+
+type Release struct {
+	Name    string `json:"name"`
+	Version string `json:"version"`
+}
+
+type IndexDeployment struct {
+	Name     string    `json:"name"`
+	Releases []Release `json:"releases"`
+}
+
+type ShowDeployment struct {
+	Manifest string `json:"manifest"`
+}
+
+var _ = Describe("Generate", func() {
+	var server *ghttp.Server
+	var session *gexec.Session
+	var outputDir string
+	var manifest string
+
+	JustBeforeEach(func() {
+		server = ghttp.NewServer()
+		var err error
+		outputDir, err = ioutil.TempDir("", "XXXXXXX")
+		Expect(err).NotTo(HaveOccurred())
+
+		deployments := []IndexDeployment{
+			{
+				Name: "cf-warden",
+				Releases: []Release{
+					{
+						Name:    "cf",
+						Version: "213+dev.2",
+					},
+				},
+			},
+			{
+				Name: "cf-warden-diego",
+				Releases: []Release{
+					{
+						Name:    "cf",
+						Version: "213+dev.2",
+					},
+					{
+						Name:    "diego",
+						Version: "0.1366.0+dev.2",
+					},
+				},
+			},
+		}
+
+		yaml, err := ioutil.ReadFile(manifest)
+		Expect(err).ToNot(HaveOccurred())
+		diegoDeployment := ShowDeployment{
+			Manifest: string(yaml),
+		}
+
+		server.AppendHandlers(
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", "/deployments"),
+				ghttp.RespondWithJSONEncoded(200, deployments),
+			),
+			ghttp.CombineHandlers(
+				ghttp.VerifyRequest("GET", "/deployments/cf-warden-diego"),
+				ghttp.RespondWithJSONEncoded(200, diegoDeployment),
+			),
+		)
+
+		command := exec.Command("../install-bat.rb", server.URL(), outputDir)
+		session, err = gexec.Start(command, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(0))
+	})
+
+	BeforeEach(func() {
+		manifest = "one_zone_manifest.yml"
+	})
+
+	AfterEach(func() {
+		Expect(os.RemoveAll(outputDir)).To(Succeed())
+	})
+
+	It("sends get requests to get the deployments", func() {
+		Expect(server.ReceivedRequests()).To(HaveLen(2))
+	})
+
+	It("sends basic auth information", func() {
+		auth := server.ReceivedRequests()[0].Header.Get("Authorization")
+		Expect(auth).To(HavePrefix("Basic "))
+		tokens := strings.Split(auth, " ")
+		Expect(tokens).To(HaveLen(2))
+		credentials, err := base64.StdEncoding.DecodeString(tokens[1])
+		Expect(err).NotTo(HaveOccurred())
+		Expect(credentials).To(BeEquivalentTo("admin:admin"))
+	})
+
+	Context("when one deployment is returned with CF+diego", func() {
+		It("generates the certificate authority cert", func() {
+			cert, err := ioutil.ReadFile(path.Join(outputDir, "ca.crt"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cert).To(BeEquivalentTo("CA_CERT"))
+		})
+
+		It("generates the client cert", func() {
+			cert, err := ioutil.ReadFile(path.Join(outputDir, "client.crt"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cert).To(BeEquivalentTo("CLIENT_CERT"))
+		})
+
+		It("generates the client key", func() {
+			cert, err := ioutil.ReadFile(path.Join(outputDir, "client.key"))
+			Expect(err).NotTo(HaveOccurred())
+			Expect(cert).To(BeEquivalentTo("CLIENT_KEY"))
+		})
+
+		Describe("the lines of the batch script", func() {
+			var lines []string
+			var script string
+
+			JustBeforeEach(func() {
+				content, err := ioutil.ReadFile(path.Join(outputDir, "install_zone1.bat"))
+				Expect(err).NotTo(HaveOccurred())
+				script = strings.TrimSpace(string(content))
+				lines = strings.Split(string(script), "\r\n")
+			})
+
+			It("contains all the MSI parameters", func() {
+				expectedContent := `msiexec /norestart /i diego.msi ^
+  ADMIN_USERNAME=[USERNAME] ^
+  ADMIN_PASSWORD=[PASSWORD] ^
+  CONSUL_IPS=consul1.foo.bar ^
+  CF_ETCD_CLUSTER=http://etcd1.foo.bar:4001 ^
+  STACK=windows2012R2 ^
+  REDUNDANCY_ZONE=zone1 ^
+  LOGGREGATOR_SHARED_SECRET=secret123 ^
+  ETCD_CA_FILE=%cd%\ca.crt ^
+  ETCD_CERT_FILE=%cd%\client.crt ^
+  ETCD_KEY_FILE=%cd%\client.key`
+				expectedContent = strings.Replace(expectedContent, "\n", "\r\n", -1)
+				Expect(script).To(Equal(expectedContent))
+			})
+		})
+
+		Context("when there is one redundancy zone", func() {
+			It("generates only one file", func() {
+				matches, err := filepath.Glob(path.Join(outputDir, "install_*.bat"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matches).To(HaveLen(1))
+				Expect(path.Join(outputDir, "install_zone1.bat")).To(BeAnExistingFile())
+			})
+		})
+
+		Context("when there is more than one redundancy zone", func() {
+			BeforeEach(func() {
+				manifest = "two_zone_manifest.yml"
+			})
+			It("generates one file per zone", func() {
+				matches, err := filepath.Glob(path.Join(outputDir, "install_*.bat"))
+				Expect(err).NotTo(HaveOccurred())
+				Expect(matches).To(HaveLen(2))
+				Expect(path.Join(outputDir, "install_zone1.bat")).To(BeAnExistingFile())
+				Expect(path.Join(outputDir, "install_zone2.bat")).To(BeAnExistingFile())
+			})
+		})
+	})
+})
