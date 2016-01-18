0eebb1ed60cfcf3a49f8e0b46b433b4388ab28e3
Matthew Horan <mhoran@pivotal.io>
2016-01-18 16:57:08 -0500

    Fix replace-version for version properties
    [#108759984]
    
    Signed-off-by: Micah Young <myoung@pivotal.io>

--- scripts/replace-version.ps1
+++ scripts/replace-version.ps1
@@ -2,7 +2,7 @@ $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 $versioninfoPath = "..\src\generate\versioninfo.json"
 push-location $dir
-   $versioninfo = (get-content $versioninfoPath) | ConvertFrom-Json
+   $versioninfo = (get-content -Raw $versioninfoPath) | ConvertFrom-Json
    $versioninfo.StringFileInfo.ProductVersion = "$env:APPVEYOR_BUILD_VERSION-$env:APPVEYOR_REPO_COMMIT"
    ConvertTo-Json $versioninfo | Set-Content $versioninfoPath 
 pop-location
